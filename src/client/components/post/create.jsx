0ea5104f64537f6f0980be6d2675e019e9fe0a35
Fei Yang <feiiyang0219@gmail.com>
2017-10-17 19:56:53 -0400

    added create post

---
+++ src/client/components/post/create.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="create-post">
+                Create a post - coming soon!
+            </div>
+        );
+    }
+}
+
+export default CreatePost;
