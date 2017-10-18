e123a234db500e0a209614d46825166f3553c97e
Fei Yang <feiiyang0219@gmail.com>
2017-10-17 20:06:18 -0400

    updated create post

--- src/client/components/post/create.jsx
+++ src/client/components/post/create.jsx
@@ -4,15 +4,29 @@ class CreatePost extends Component {
 
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlePostChange = this.handlePostChange.bind(this);
+    }
+
+    handlePostChange(event) {
+        console.log('handling an update to the post body');
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('handling submission');
     }
 
     render() {
         return (
-            <div className="create-post">
-                Create a post - coming soon!
-            </div>
+            <form className="create-post">
+                <textarea placeholder="What's on your mind" />
+                <input type="submit" placeholder="Post" className="btn btn-default"/>
+            </form>
         );
     }
 }
+
+CreatePost.propTypes = {};
 
 export default CreatePost;
