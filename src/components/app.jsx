8ef1014226a1c92acff4beebbae6e86c288e8d8c
Fei Yang <feiiyang0219@gmail.com>
2017-10-20 23:34:38 -0400

    added ajax call to back end

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,34 @@ import CreatePost from './post/create';
 
 class App extends Component {
 
+    handlePostSubmit(payload) {
+        if (!payload.content) {
+            return;
+        }
+
+        const oldPosts = this.state.posts;
+        oldPosts.unshift(payload);
+
+        this.setState({
+            posts: oldPosts, 
+
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+
+        fetch('{process.env.ENDPOINT}/posts', requestOptions)
+            .then(res => {
+                if (res.ok === true) {
+                    this.fetchPosts();
+                }
+            });
+    }
 
     render() {
         return (
